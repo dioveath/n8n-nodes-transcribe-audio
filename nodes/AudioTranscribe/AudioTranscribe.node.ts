084dfec6011a0b97523ebfc0379ff701fc808b37
Saroj Rai <raisaroj360@gmail.com>
2025-08-11 05:39:56 +0545

    add: refactored the node to work without unnecessary requirements

--- nodes/AudioTranscribe/AudioTranscribe.node.ts
+++ nodes/AudioTranscribe/AudioTranscribe.node.ts
@@ -1,5 +1,4 @@
 import type {
-	IBinaryData,
 	IExecuteFunctions,
 	INodeExecutionData,
 	INodeType,
@@ -8,36 +7,20 @@ import type {
 import { NodeConnectionType, NodeOperationError } from 'n8n-workflow';
 
 import { WaveFile } from 'wavefile';
-import { Readable } from 'stream';
+import { pipeline, env } from '@huggingface/transformers';
 
-const ffmpeg = require('fluent-ffmpeg');
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffprobePath = require('@ffprobe-installer/ffprobe').path;
+import path from 'path'
+const wasmDir = path.dirname(require.resolve('onnxruntime-web'));
 
-ffmpeg.setFfmpegPath(ffmpegPath);
-ffmpeg.setFfprobePath(ffprobePath);
-
-type TransformersModule = typeof import('@huggingface/transformers');
-let transformersPromise: Promise<TransformersModule> | null = null;
-
-async function getInitializedTransformers(): Promise<TransformersModule> {
-	if (transformersPromise === null) {
-		transformersPromise = (async () => {
-			const T = await import('@huggingface/transformers');
-			T.env.allowLocalModels = true;
-			T.env.useBrowserCache = false;
-			return T;
-		})();
-	}
-	return transformersPromise;
+if (env.backends.onnx.wasm) {
+	env.backends.onnx.wasm.wasmPaths = wasmDir + path.sep;
 }
 
 const MODELS_LIST = [
 	'Xenova/whisper-tiny.en',
 	'Xenova/whisper-base.en',
 	'Xenova/whisper-small.en',
-	'Xenova/whisper-medium.en', // Medium and larger models can be demanding on CPU
-	// You can add multilingual models too, e.g., 'Xenova/whisper-tiny' (without .en)
+	'Xenova/whisper-medium.en',
 	// 'Xenova/whisper-large-v3', // Very resource-intensive
 ];
 
@@ -130,6 +113,7 @@ export class AudioTranscribe implements INodeType {
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
+		const returnData: INodeExecutionData[] = [];
 
 		let item: INodeExecutionData;
 		let operation: string;
@@ -137,12 +121,6 @@ export class AudioTranscribe implements INodeType {
 		let binaryPropertyName: string;
 		let model: string;
 
-		const { pipeline } = await getInitializedTransformers();
-
-
-		// Iterates over all input items and add the key "myString" with the
-		// value the parameter "myString" resolves to.
-		// (This could be a different value for each item in case it contains an expression)
 		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
 			try {
 				operation = this.getNodeParameter('operation', itemIndex, '') as string;
@@ -157,103 +135,94 @@ export class AudioTranscribe implements INodeType {
 						if (item.binary === undefined) {
 							throw new NodeOperationError(this.getNode(), `No binary data found on item!`, { itemIndex });
 						}
+
 						this.logger.info(`Attempting to load model: "${model}" for item index ${itemIndex}`);
 						const transcriber = await pipeline('automatic-speech-recognition', model, {
-							progress_callback: (_progress: any) => {}
+							progress_callback: (_progress: any) => { },
 						});
-						const binaryItem = item.binary[binaryPropertyName] as IBinaryData;
+						this.logger.info(`Model "${model}" loaded successfully for item index ${itemIndex}`);
 
-						if (typeof binaryItem.data !== 'string') {
-							throw new NodeOperationError(this.getNode(), `Expected binary data to be a string! Received type: ${typeof binaryItem.data}`, { itemIndex });
-						}
-						
+						const buffer = await this.helpers.getBinaryDataBuffer(itemIndex, binaryPropertyName);
+
 						// const tempDir = os.tmpdir()
 						// const tempFilePath = path.join(tempDir, `audio_${itemIndex}_${Date.now()}.${binaryData.fileExtension || "wav"}`)
-						const buffer = Buffer.from(binaryItem.data, 'base64');
+						// const buffer = Buffer.from(binaryItem.data, 'base64');
 						// let url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/jfk.wav';
 						// const response = await fetch(url);
 						// const arrayBuffer = await response.arrayBuffer();
 						// const buffer = Buffer.from(new Uint8Array(arrayBuffer));
 
-						const mimeType = binaryItem.mimeType
-						const fileExtension = binaryItem.fileExtension
-						let audioBufferWav: Buffer
+						// const mimeType = binaryItem.mimeType
+						// const fileExtension = binaryItem.fileExtension
+						// let audioBufferWav: Buffer
 
-						if (mimeType === 'audio/mpeg' || mimeType === 'audio/mp3' || fileExtension === 'mp3') {
-							const readableMp3Stream = Readable.from(buffer);
-							audioBufferWav = await new Promise<Buffer>((resolve, reject) => {
-								const chunks: Uint8Array[] = [];
-								ffmpeg(readableMp3Stream)
-									.format('wav')
-									.audioCodec('pcm_s16le')
-									.audioFrequency(16000)
-									.audioChannels(1)
-									.on('start', (commnadLine: string) => {
-										this.logger.info(`Conversion started: ${commnadLine}`);
-									})
-									.on('error', (err: Error, stdout: string, stderr: string) => {
-										this.logger.error(`Std out: ${stdout}`);
-										this.logger.error(`Std err: ${stderr}`);
-										reject(new NodeOperationError(this.getNode(), `Conversion failed: ${err.message}`, { itemIndex }));
-									})
-									.on('end', (stdout: string, stderr: string) => {
-										resolve(Buffer.concat(chunks));
-									})
-									.pipe()
-									.on('data', (data: Uint8Array) => {
-										chunks.push(data);
-									});
-							})							
-						} else {
-							this.logger.info(`Input (item ${itemIndex}, mime: ${mimeType}, ext: ${fileExtension}) assumed to be WAV. Using directly.`);
-							audioBufferWav = buffer;
-						}
+						// if (mimeType === 'audio/mpeg' || mimeType === 'audio/mp3' || fileExtension === 'mp3') {
+						// 	const readableMp3Stream = Readable.from(buffer);
+						// 	audioBufferWav = await new Promise<Buffer>((resolve, reject) => {
+						// 		const chunks: Uint8Array[] = [];
+						// 		ffmpeg(readableMp3Stream)
+						// 			.format('wav')
+						// 			.audioCodec('pcm_s16le')
+						// 			.audioFrequency(16000)
+						// 			.audioChannels(1)
+						// 			.on('start', (commnadLine: string) => {
+						// 				this.logger.info(`Conversion started: ${commnadLine}`);
+						// 			})
+						// 			.on('error', (err: Error, stdout: string, stderr: string) => {
+						// 				this.logger.error(`Std out: ${stdout}`);
+						// 				this.logger.error(`Std err: ${stderr}`);
+						// 				reject(new NodeOperationError(this.getNode(), `Conversion failed: ${err.message}`, { itemIndex }));
+						// 			})
+						// 			.on('end', (stdout: string, stderr: string) => {
+						// 				resolve(Buffer.concat(chunks));
+						// 			})
+						// 			.pipe()
+						// 			.on('data', (data: Uint8Array) => {
+						// 				chunks.push(data);
+						// 			});
+						// 	})							
+						// } else {
+						// 	this.logger.info(`Input (item ${itemIndex}, mime: ${mimeType}, ext: ${fileExtension}) assumed to be WAV. Using directly.`);
+						// 	audioBufferWav = buffer;
+						// }
 
-						const wav = new WaveFile(audioBufferWav);
+						const wav = new WaveFile(buffer);
 						wav.toBitDepth('32f'); // pipeline expects input as a Float32Array
 						wav.toSampleRate(16000); // pipeline expects input at 16kHz
 
-						try {
-							let audioData = wav.getSamples();
-							if (Array.isArray(audioData)) {
-								if (audioData.length > 0) {
-									const SCALING_FACTOR = Math.sqrt(2);
-									for (let i = 0; i < audioData.length; i++) {
-										audioData[0][i] = SCALING_FACTOR * (audioData[0][i] + audioData[1][i]) / 2;
-									}
+						let audioData = wav.getSamples();
+						if (Array.isArray(audioData)) {
+							if (audioData.length > 0) {
+								const SCALING_FACTOR = Math.sqrt(2);
+								for (let i = 0; i < audioData.length; i++) {
+									audioData[0][i] = SCALING_FACTOR * (audioData[0][i] + audioData[1][i]) / 2;
 								}
-								audioData = audioData[0]
 							}
-
-							let start = performance.now()
-							let result = await transcriber(audioData)
-							let end = performance.now()
-							this.logger.info(`Transcription took ${end - start}ms`)
-
-							item.json.transcription = result;
-						} catch (error) {
-							throw error
-						} finally {
-							// try {
-							// 	await fs.promises.unlink(tempFilePath)
-							// } catch (e) {
-							// 	// Ignore error, we don't want to fail the node if we can't delete the file
-							// }
+							audioData = audioData[0]
 						}
 
+						this.logger.info(`Starting transcription for item index ${itemIndex}`)
+						let start = performance.now()
+						let result = await transcriber(audioData, { chunk_length_s: 30, stride_length_s: 5 })
+						let end = performance.now()
+						this.logger.info(`Transcription took ${end - start}ms`)
+
+						const newItem = { ...item, json: { ...item.json } };
+						newItem.json.transcription = result;
+						returnData.push(newItem)
 					}
 				}
 
 			} catch (error) {
-				// This node should never fail but we want to showcase how
-				// to handle errors.
 				if (this.continueOnFail()) {
-					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+					const errorItem = {
+						json: { ...items[itemIndex].json },
+						error,
+						pairedItem: itemIndex,
+					};
+					returnData.push(errorItem);
 				} else {
-					// Adding `itemIndex` allows other workflows to handle this error
 					if (error.context) {
-						// If the error thrown already contains the context property,
-						// only append the itemIndex
 						error.context.itemIndex = itemIndex;
 						throw error;
 					}
@@ -262,8 +231,9 @@ export class AudioTranscribe implements INodeType {
 					});
 				}
 			}
+
 		}
 
-		return [items];
+		return this.prepareOutputData(returnData);
 	}
 }
